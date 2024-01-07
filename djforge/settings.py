beabffd7abc9e66706d62db73e28a279437d362c
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-07 17:21:18 +0000

    Remove comment, not needed

--- djforge/settings.py
+++ djforge/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "djforge.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],  # new
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
