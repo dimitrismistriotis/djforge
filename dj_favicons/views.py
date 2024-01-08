dd27009a6a7f504ae935c738f6c10b9cf4b27821
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-08 10:29:40 +0000

    Favicon response.

--- dj_favicons/views.py
+++ dj_favicons/views.py
@@ -1,4 +1,16 @@
 """Views for the dj_favicons app."""
+from pathlib import Path
+
+from django.http import HttpRequest
+from django.http import FileResponse
 # from django.shortcuts import render
 
-# Create your views here.
+_FAVICON_LOCATION = Path(__file__).parent / "static" / "dj_favicons" / "favicon.ico"
+
+
+def favicon(_request: HttpRequest) -> FileResponse:
+    """Return a favicon.ico file.
+
+    Follows approach discussed here: https://stackoverflow.com/a/65990187/1622
+    """
+    return FileResponse(_FAVICON_LOCATION.open("rb"))
