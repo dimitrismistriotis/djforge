a31fddd429a3bbd39662747ad10e48ace6015f42
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-09 10:40:06 +0000

    First test to test application's ability to present a favicon.

--- dj_favicons/tests/test_views.py
+++ dj_favicons/tests/test_views.py
@@ -1,10 +1,12 @@
 """Test for the views of the dj_favicons app."""
+from django.test import Client
 
 
 class TestFaviconViews:
     """Test for the favicon view."""
 
-    def test_favicon(self, client) -> None:
+    def test_favicon(self, client: Client) -> None:
         """Test the favicon view."""
         response = client.get("/favicon.ico")
+
         assert response.status_code == 200
