5ebaddd1014180ae5386d5518117b38f4dad2e13
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-10 10:03:36 +0000

    poetry run python manage.py startapp dj_landing_page

---
+++ dj_landing_page/apps.py
@@ -0,0 +1,9 @@
+"""AppConfig for dj_landing_page app."""
+from django.apps import AppConfig
+
+
+class DjLandingPageConfig(AppConfig):
+    """AppConfig for dj_landing_page app."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dj_landing_page"
---
+++ dj_landing_page/views.py
@@ -0,0 +1,4 @@
+"""Views for dj_landing_page app."""
+# from django.shortcuts import render
+
+# Create your views here.
