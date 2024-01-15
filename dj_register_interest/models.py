b3fe5acbdf5f5a8ce2b4a42a200c6b0818cf5588
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-15 09:54:21 +0000

    feat(Register-Interest): Main model and migration.

--- dj_register_interest/models.py
+++ dj_register_interest/models.py
@@ -1,4 +1,31 @@
 """Models for dj_register_interest app."""
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Interest(models.Model):
+    """Model for a user's interest in the project."""
+
+    name = models.CharField(
+        verbose_name="(Optional) name of person registering interest",
+        max_length=100,
+        blank=True,
+    )
+    email = models.EmailField(
+        verbose_name=(
+            "Email address to use to communicate solution's availability to user"
+        )
+    )
+    github_handle = models.CharField(
+        verbose_name="Github Handle in case solutions grants access a repository",
+        max_length=100,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return f"Interest of {self.name} ({self.email}), Github: {self.github_handle}"
+
+    def __repr__(self) -> str:
+        return (
+            f"Interest(name={self.name}, "
+            f"email={self.email}, github_handle={self.github_handle})"
+        )
