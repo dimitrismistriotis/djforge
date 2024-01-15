3c7cb21f3a96edb669fa15b933cb736abf447065
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-15 09:59:16 +0000

    feat(Register-Interest): Class based view - synthetic, added hinting.

--- dj_register_interest/views.py
+++ dj_register_interest/views.py
@@ -1,4 +1,28 @@
 """Views for dj_register_interest app."""
-# from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import InterestForm
+
+
+class RegisterInterestView(View):
+    """View for registering interest in the project."""
+
+    form_class = InterestForm
+    template_name = "register_interest.html"
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        """Get request for the view."""
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs) -> HttpResponseRedirect | HttpResponse:
+        """Post request for the view."""
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("success_url")  # Redirect to a success page
+
+        return render(request, self.template_name, {"form": form})
