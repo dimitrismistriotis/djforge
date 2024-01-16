7ca40fd2fcc375e8e30413329621dff4966e5459
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-16 13:31:28 +0000

    feat(Register-Interest): Add logging.

--- dj_register_interest/handlers.py
+++ dj_register_interest/handlers.py
@@ -1,4 +1,6 @@
 """Register Interest Handlers."""
+import logging
+
 from .forms import InterestForm
 
 
@@ -8,8 +10,12 @@ class RegisterInterestHandler:
     Can be expanded to notify project's stakeholders with email, text message, etc.
     """
 
+    _logger = logging.getLogger(__name__)
+
     def __call__(self, form: InterestForm) -> bool:
         """Entry point"""
+        self._logger.info("Saving interest form data %s", form.data)
+
         if form.is_valid():
             form.save()
             return True
