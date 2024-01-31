7568d7b4724010dbce678b429923b3f2e5fe0756
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-01-31 17:47:00 +0000

    feat(UI-Django-Messages): Placeholders to create different messasges and display

--- dj_theme/views.py
+++ dj_theme/views.py
@@ -1,4 +1,9 @@
 """Views for dj_theme app."""
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
 
-# Create your views here.
+
+def mesages_demo(request: HttpRequest) -> HttpResponse:
+    """Display different messages to demonstrate their UI."""
+    return render(request, "dj_theme/demo_display_messages.html")
