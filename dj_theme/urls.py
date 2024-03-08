615b9b10c19932fcac42e69c1c21693caae633ca
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-08 17:27:36 +0000

    build(Dashboard): display theme urls only on debug mode

--- dj_theme/urls.py
+++ dj_theme/urls.py
@@ -1,6 +1,7 @@
 """Urls for registering interest in the DJ app."""
 
 from django.urls import path
+from django.conf import settings
 
 from .views import messages_demo
 from .views import logging_demo
@@ -8,8 +9,16 @@ from .views import dashboard_template
 
 app_name = "dj_theme"
 
-urlpatterns = [
-    path("messages", messages_demo, name="messages_demo"),
-    path("logging", logging_demo, name="logging_demo"),
-    path("dashboard", dashboard_template, name="dashboard"),
-]
+
+#
+# Theme URLs should be available only in DEBUG mode / development environments.
+#
+urlpatterns = (
+    [
+        path("messages", messages_demo, name="messages_demo"),
+        path("logging", logging_demo, name="logging_demo"),
+        path("dashboard", dashboard_template, name="dashboard"),
+    ]
+    if getattr(settings, "DEBUG", False)
+    else []
+)
