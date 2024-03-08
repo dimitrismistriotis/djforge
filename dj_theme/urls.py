3088f0503a432345b541372199d352e467dbf460
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-08 17:25:17 +0000

    build(Dashboard): move template to theme to be reused from there

--- dj_theme/urls.py
+++ dj_theme/urls.py
@@ -1,12 +1,15 @@
 """Urls for registering interest in the DJ app."""
+
 from django.urls import path
 
 from .views import messages_demo
 from .views import logging_demo
+from .views import dashboard_template
 
 app_name = "dj_theme"
 
 urlpatterns = [
     path("messages", messages_demo, name="messages_demo"),
     path("logging", logging_demo, name="logging_demo"),
+    path("dashboard", dashboard_template, name="dashboard"),
 ]
