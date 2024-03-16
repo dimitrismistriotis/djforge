93fdc86304002f406252fd2dd75018c658d47297
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-16 17:39:05 +0000

    test(SignUpURL): test, needs some fixing

---
+++ dj_dashboard/tests/test_sign_up_redirection_to_dashboard_with_flag.py
@@ -0,0 +1,37 @@
+"""Test Sign Up Redirects Users to Dashboard with Specific Flag."""
+
+import pytest
+
+from django.urls import reverse
+
+pytestmark = [pytest.mark.django_db]
+
+
+class TestUserSignupRedirect:
+    """Test Sign Up Redirects Users to Dashboard with Specific Flag."""
+
+    SIGNUP_URL = reverse("account_signup")
+
+    # noinspection HardcodedPassword
+    def test_signup_redirects_to_dashboard_on_success(self, client):
+        """Test that a successful signup redirects the user to "/dashboard?signup"."""
+        user_details = {
+            "username": "user@example.com",
+            "password1": "ReallySecureP4$$word!",
+            "password2": "ReallySecureP4$$word!",
+            "email": "user@example.com",
+            # Add other fields required by your signup form
+        }
+        response = client.post(self.SIGNUP_URL, user_details, follow=True)
+
+        # Check if the response redirected to "/dashboard?signup".
+        dashboard_url = (
+            reverse("dj_dashboard:dashboard") + "?signup"
+        )  # Adjust if your URL pattern for dashboard is different
+        assert response.status_code == 200
+        assert (
+            response.request["PATH_INFO"] + "?" + response.request["QUERY_STRING"]
+            == dashboard_url
+        )
+
+        assert response.context["user"].is_authenticated
