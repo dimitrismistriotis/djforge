0a123618d9cc2f086b3113b65b9bec1b4c6817e4
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-17 13:36:17 +0000

    feat(Dashboard): conditional signup box with instructions

--- dj_dashboard/views.py
+++ dj_dashboard/views.py
@@ -9,4 +9,13 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request: HttpRequest) -> HttpResponse:
     """Return a Dashboard page, to use in development while integrating."""
-    return render(request, "dj_dashboard/dashboard.html")
+    from_sign_up = request.path.endswith("signup")
+    # print(f"{from_sign_up=}")
+
+    return render(
+        request,
+        "dj_dashboard/dashboard.html",
+        {
+            "from_sign_up": from_sign_up,
+        },
+    )
