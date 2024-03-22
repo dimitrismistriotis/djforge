dd770142305e4d45bb35e6b126c9f71cd239f53f
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-22 06:15:14 +0000

    docs(Celery): give calls in tasks with explanation

--- djforge/celery.py
+++ djforge/celery.py
@@ -5,6 +5,7 @@ https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
 ... where requested to create a file within the module named celery.py and confgure
 the Celery app there.
 """
+
 import os
 
 from celery import Celery
@@ -26,5 +27,19 @@ app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
-    """Debug task to print request information."""
+    """Debug task to print request information.
+
+    >>> from djforge.celery import debug_task
+    >>> task_result = debug_task.apply_async()
+
+    Then:
+
+    >>> task_result.id
+    '1320d679-6e99-4dad-a958-92bf02857646'
+
+    >>> task_result.status
+    'PENDING'
+
+    Result status will be always PENDING because ignore_result=True.
+    """
     print(f"Request: {self.request!r}")
