363398f575f964b07ff64f8b56502e2763f37669
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-03-29 14:32:09 +0000

    feat(Health Endpoint): introduce endpoint

--- dj_content/urls.py
+++ dj_content/urls.py
@@ -3,9 +3,11 @@
 from django.urls import path
 
 from .views import about_us
+from .views import health
 
 app_name = "dj_content"
 
 urlpatterns = [
     path("about_us", about_us, name="about-us"),
+    path("health", health, name="health"),
 ]
