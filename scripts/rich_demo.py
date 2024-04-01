0e7b944481facfb00746fdaeac09b6f657b7712e
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-04-01 18:16:55 +0100

    feat(Scripts): placeholder with documentation on how to call scripts

---
+++ scripts/rich_demo.py
@@ -0,0 +1,15 @@
+"""Demo Rich features.
+
+Can also be used as a template for other scripts.
+
+Run with:
+
+```shell
+poetry run python manage.py runscript rich_demo --script-args argument1 argument2
+```
+"""
+
+
+def run(*args) -> None:
+    """Show different Rich features."""
+    print(args)
