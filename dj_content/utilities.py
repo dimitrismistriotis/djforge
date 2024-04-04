62d4d937f2853f0a03720c8ae09fa528ba9ddc52
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-04-04 20:05:01 +0100

    doc(License): document utility call

--- dj_content/utilities.py
+++ dj_content/utilities.py
@@ -7,6 +7,10 @@ def read_file_into_array(file_path) -> list[str]:
     """Read a file into an array of lines.
 
     Note: Should move to a "core" or "utilities" app if to be used in multiple apps.
+
+    >>> from dj_content.utilities import read_file_into_array
+    >>> file_path = Path("path/to/file.txt")
+    >>> lines = read_file_into_array(file_path)
     """
     file_path = Path(file_path)
     file_contents = file_path.read_text()
