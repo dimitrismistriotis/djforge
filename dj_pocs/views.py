c9a4039210f824eaa3a051a5cc818202584bb4db
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-04-07 19:02:24 +0100

    feat(Poc-App): display message if no map api key is available

--- dj_pocs/views.py
+++ dj_pocs/views.py
@@ -1,5 +1,6 @@
 """Views for the dj_pocs app."""
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.http import HttpRequest
 from django.shortcuts import render
@@ -7,4 +8,7 @@ from django.shortcuts import render
 
 def map_and_postcode_analysis(request: HttpRequest) -> HttpResponse:
     """Display a map and a table with postcode analysis."""
-    return render(request, "dj_pocs/map.html")
+    google_maps_api_key = getattr(settings, "GOOGLE_MAPS_API_KEY", None)
+    return render(
+        request, "dj_pocs/map.html", {google_maps_api_key: google_maps_api_key}
+    )
