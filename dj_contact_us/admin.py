434bf17f0ed413958f5c5e8e5f3cc0fbacfb16a3
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-04-23 20:19:52 +0100

    feat(Contact-Us): admin display

--- dj_contact_us/admin.py
+++ dj_contact_us/admin.py
@@ -1,4 +1,24 @@
 """Admin configuration for the dj_contact_us app."""
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import ContactUsEntry
+
+
+class ContactUsEntryAdmin(admin.ModelAdmin):
+    """Admin class for ContactUsEntry model."""
+
+    list_display = (
+        "name",
+        "email",
+        "message",
+        "created_at",
+    )
+    search_fields = (
+        "email",
+        "name",
+    )
+
+    ordering = ("-id",)
+
+
+admin.site.register(ContactUsEntry, ContactUsEntryAdmin)
