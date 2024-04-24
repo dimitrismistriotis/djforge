8bb627ad992934041b33c8341cbc064cd7c51f3d
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-04-24 19:33:32 +0100

    feat(Contact-Us): add to urls

--- dj_contact_us/urls.py
+++ dj_contact_us/urls.py
@@ -1,8 +1,11 @@
 """Urls for Contact Us of the DJ app."""
 
-# from django.urls import path
+from django.urls import path
 
+from .views import contact_view
 
 app_name = "dj_contact_us"
 
-urlpatterns = []
+urlpatterns = [
+    path("", contact_view, name="contact-us"),
+]
