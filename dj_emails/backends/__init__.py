755f849cc249e67c75769b55d25032e4dc73c2d3
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-05-13 19:54:36 +0100

    feat(Email-Resend): Boilerplate

---
+++ dj_emails/backends/__init__.py
@@ -0,0 +1,1 @@
+"""__init__ of backends for dj_emails."""
---
+++ dj_emails/backends/resend.py
@@ -0,0 +1,42 @@
+"""Django backend for Resend.com."""
+from django.core.mail.backends.base import BaseEmailBackend
+
+"""Base email backend class."""
+
+
+class ResendEmailBackend(BaseEmailBackend):
+    """Dispatch Emails through Resend.com.
+
+    As a subclass of BaseEmailBackend, it must at least overwrite send_messages().
+
+    open() and close() can be called indirectly by using a backend object as a
+    context manager:
+
+       with backend as connection:
+           # do something with connection
+           pass
+    """
+
+    def __init__(self, fail_silently=False, **kwargs):
+        """Initialize the ResendEmailBackend class.
+
+        Check the exisence of an API key and fail if it does not exist.
+        """
+        super().__init__(fail_silently=fail_silently, **kwargs)
+
+    def open(self):
+        """Override the open method of BaseEmailBackend, but does nothing."""
+        pass
+
+    def close(self):
+        """Override the close method of BaseEmailBackend, but does nothing."""
+        pass
+
+    def send_messages(self, email_messages) -> int:
+        """Send one or more EmailMessage objects.
+
+        Returns the number of email messages sent.
+        """
+        raise NotImplementedError(
+            "subclasses of BaseEmailBackend must override send_messages() method"
+        )
