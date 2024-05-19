9d1b9465dec1d2f7ac4fc951d6e8d5790fa8e3ea
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-05-19 22:19:23 +0100

    tests(Contact-Us): clean up test and check for email dispatch

--- dj_contact_us/tests/test_content_view.py
+++ dj_contact_us/tests/test_content_view.py
@@ -3,6 +3,8 @@ import pytest
 
 from django.urls import reverse
 from django.core import mail
+from django.test import Client
+from django.test import override_settings
 
 from ..models import ContactUsEntry
 
@@ -26,22 +28,24 @@ class TestContactView:
         "message": "",
     }
 
-    def test_contact_view_get(self, client) -> None:
+    @override_settings(DISPATCHING_EMAILS=False)  # Does not matter for this test
+    def test_contact_view_get(self, client: Client) -> None:
         """Test dropped from synthetic, in progress."""
         response = client.get(self.TARGET_URL)
 
         assert response.status_code == 200
 
-    def test_contact_view_post_valid_data(self, client) -> None:
+    @override_settings(DISPATCHING_EMAILS=False)  # Does not matter for this test
+    def test_contact_view_post_valid_data(self, client: Client) -> None:
         """Test dropped from synthetic, in progress."""
         response = client.post(self.TARGET_URL, self._CORRECT_QUERY_DATA)
 
         assert response.status_code == 302
         assert response.url == self.TARGET_URL
         assert ContactUsEntry.objects.count() == 1
-        # assert len(mail.outbox) == 2  # One email to the user, one to the admin
 
-    def test_contact_view_post_invalid_data(self, client) -> None:
+    @override_settings(DISPATCHING_EMAILS=False)  # Does not matter for this test
+    def test_contact_view_post_invalid_data(self, client: Client) -> None:
         """Test dropped from synthetic, in progress."""
         data = {"name": "", "email": "invalid-email", "message": ""}
 
@@ -51,26 +55,20 @@ class TestContactView:
         assert ContactUsEntry.objects.count() == 0
         # assert len(mail.outbox) == 0
 
-    def test_email_sent(self, client) -> None:
+    @override_settings(DISPATCHING_EMAILS=True)
+    def test_email_sent(self, client: Client) -> None:
         """Test that an email is sent when the view is called."""
-        # Arrange
-
-        # Act
         client.post(self.TARGET_URL, self._CORRECT_QUERY_DATA)
 
-        # Assert
-        assert len(mail.outbox) == 1  # Check if one email was sent
-        # assert mail.outbox[0].subject == data["subject"]  # Check if the subject matches
+        # assert len(mail.outbox) == 2  # One email to the user, one to the admin
+        assert mail.outbox[0].subject == "New Contact Form Submission"
         assert (
             self._CORRECT_QUERY_DATA["email"] in mail.outbox[0].to
         )  # Check if the recipient is correct
 
-    def test_no_email_sent_with_invalid_data(self, client) -> None:
+    @override_settings(DISPATCHING_EMAILS=True)
+    def test_no_email_sent_with_invalid_data(self, client: Client) -> None:
         """Test that no email is sent when invalid data is provided."""
-        # Arrange
-
-        # Act
         client.post(self.TARGET_URL, self._INVALID_QUERY_DATA)
 
-        # Assert
         assert len(mail.outbox) == 0  # Check that no email was sent
