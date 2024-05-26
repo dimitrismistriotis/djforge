2d7cee84314bbc1efc7fbbbdb1e3fffce33553f2
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-05-26 11:55:04 +0100

    refactor(Permissions): remove from app config as Django does not want db io at this stage

--- dj_users/apps.py
+++ dj_users/apps.py
@@ -7,11 +7,3 @@ class DjUsersConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "dj_users"
-
-    def ready(self) -> None:
-        """When app is ready execute the following."""
-        super().ready()
-
-        from .seed import create_platform_administrators_group
-
-        create_platform_administrators_group()
