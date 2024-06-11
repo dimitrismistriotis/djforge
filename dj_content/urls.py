93b6bcedf5559562d178b9c4c9026c4c5efdf076
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2024-06-11 05:48:46 +0100

    feat(Cookie-Policy): add document

--- dj_content/urls.py
+++ dj_content/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 
 from .views import about_us
+from .views import cookies_policy
 from .views import health
 from .views import license
 
@@ -10,6 +11,7 @@ app_name = "dj_content"
 
 urlpatterns = [
     path("about_us", about_us, name="about-us"),
+    path("cookies_policy", cookies_policy, name="cookies-policy"),
     path("health", health, name="health"),
     path("license", license, name="license"),
 ]
