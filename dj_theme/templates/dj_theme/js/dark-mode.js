24520cb82c1650a25975247a465d8a4522d857a3
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2026-02-11 20:12:06 +0000

    Update to use night of daisyUI

--- dj_theme/templates/dj_theme/js/dark-mode.js
+++ dj_theme/templates/dj_theme/js/dark-mode.js
@@ -33,9 +33,11 @@
         if (localStorage.getItem("color-theme")) {
             if (localStorage.getItem("color-theme") === "light") {
                 document.documentElement.classList.add("dark");
+                document.documentElement.setAttribute("data-theme", "night");
                 localStorage.setItem("color-theme", "dark");
             } else {
                 document.documentElement.classList.remove("dark");
+                document.documentElement.setAttribute("data-theme", "light");
                 localStorage.setItem("color-theme", "light");
             }
 
@@ -43,9 +45,11 @@
         } else {
             if (document.documentElement.classList.contains("dark")) {
                 document.documentElement.classList.remove("dark");
+                document.documentElement.setAttribute("data-theme", "light");
                 localStorage.setItem("color-theme", "light");
             } else {
                 document.documentElement.classList.add("dark");
+                document.documentElement.setAttribute("data-theme", "night");
                 localStorage.setItem("color-theme", "dark");
             }
         }
